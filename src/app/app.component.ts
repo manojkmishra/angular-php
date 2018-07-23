c6a39908ae42c65635191445549163d429c65876
manojkmishra <manojmishra571980@gmail.com>
2018-07-23 18:07:57 +1000

    ngFor, table, css in table records

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,13 @@
 import { Component } from '@angular/core';
-
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  constructor() {
-  this.title = '' ;
-  setInterval(() => this.title = Math.random().toString(), 500) ;
-}
-clickFunction() {
-  console.log('button clicked');
-}
-// tslint:disable-next-line:member-ordering
-title1 = 'app1'; title2 = 'app2';
-updateTitle1(e) {
-  console.log('updateTitile1---even-e=', e.target.value);
-  this.title1 = e.target.value;
-}
-
-
+title = 'app'; title1 = 'app1'; title2 = 'app2';
+data1 = [{name: 'name1', online: true}, {name: 'name2', online: true}, {name: 'name3', online: true} ] ;
+constructor() {  this.title = '' ;  setInterval(() => this.title = Math.random().toString(), 500) ; }
+clickFunction() {  console.log('button clicked'); }
+updateTitle1(e) {  console.log('updateTitile1---even-e=', e.target.value);  this.title1 = e.target.value; }
 }
