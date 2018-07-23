4b19058a4bab6a4f86757be2a9e2b59ab7952c02
manojkmishra <manojmishra571980@gmail.com>
2018-07-23 11:13:22 +1000

    decorator created- value over ridden

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 
+function dlog(target, name, descriptor) {
+console.log('/src/app/app.component.ts--target, name, descriptor=', target, name, descriptor );
+// store the original function in a variable
+ const original = descriptor.value;
+// do some manipulations with descriptor value ----it will orver ride the anymethod written below- when dlog is called in @component
+ descriptor.value = function() { console.log('descriptor value changed' ); } ;
+ original();
+// return the descriptor
+ return descriptor;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +18,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    this.anymethod();
+  }
+  @dlog
+  anymethod() {
+    console.log('/src/app/app.component.ts--hello in dlog');
+  }
 }
