ec54095056dc3de7bd0baed3d229273d73377444
manojkmishra <manojmishra571980@gmail.com>
2018-07-24 11:02:51 +1000

    Home and Data component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { HelloComponent } from './hello/hello.component';
 import {FormsModule} from '@angular/forms';
 import { RecserviceService } from './recservice.service' ;
 import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { DataComponent } from './data/data.component';
 
 @NgModule({
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, DataComponent ],
   imports: [ BrowserModule,  FormsModule , HttpClientModule],
   providers: [RecserviceService],
   bootstrap: [AppComponent]
